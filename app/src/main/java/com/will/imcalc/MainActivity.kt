37a73f4a66b02bd3dda53827803f3c89a222fc91
Willian Brandão <willian.ramos@aluno.cefet-rj.br>
2021-04-29 19:34:31 -0300

    "referencing colors, create icons"

--- app/src/main/java/com/will/imcalc/MainActivity.kt
+++ app/src/main/java/com/will/imcalc/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.w( "lifecycle", "entrei no create - estou criando tela")
-        finish()
+
     }
     override fun onStart()
     {
